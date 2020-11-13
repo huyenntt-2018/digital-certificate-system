9f0e9087dd238e426ea2298cb6eca415368fe5fd
huyencntt-2018 <nguyen.thi.thanh.huyenc@sun-asterisk.com>
2020-11-13 11:21:55 +0700

    create router api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'API'], function() {
+    Route::get('/{user}', 'HomeController@index');
+    // đăng ký chứng thư gốc
+    Route::apiResource('register-request', 'RegisterRequestController')->except(['update, destroy']);
+    // download chứng thư và file lưu trữ cert + khóa bí mật
+    Route::get('download-cert/{id}', 'HomeController@download')->name('download-cert');
+    Route::get('download-pkcs12/{id}', 'HomeController@download')->name('download-pkcs12');
+    // ktra chứng thư
+    Route::get('check-cert/{user}/{serialNumber}', 'HomeController@checkCert')->name('check-cert');
+    // thu hồi chứng thư
+    Route::post('revoke/{user}', 'RevokeController@revoke')->name('revoke');
+    // đăng ký chứng thư tạm thời
+    Route::apiResource('intro-requests', 'IntroductionController')->only('store', 'index');
+    // tạo chữ ký và xác thực chữ ký
+    Route::post('sign', 'CertificateController@sign')->name('sign');
+    Route::post('verify', 'CertificateController@verify')->name('verify');
 });
